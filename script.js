f8babc8e41429305e0df57e43e9a2eb87554f911
Julia <julia_chung@tdchristian.ca>
2025-05-13 12:28:14 -0400

    Updated store

--- script.js
+++ script.js
@@ -23,8 +23,8 @@ const openGameScreen = () => {
 };
 
 const spawnCustomer = () => {
-  let left = Tools.random(30,65);
-  let top = Tools.random(30,72);
+  let left = Tools.random(0,100);
+  let top = Tools.random(0,100);
 
   let html = `<img src="${customerURL}" class="customer" style="left: ${left}%; top: ${top}%;">`
   $("#customerLayer").append(html);
